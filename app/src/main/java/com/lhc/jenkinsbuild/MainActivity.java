f7487a3bea25a71e09a7cbae67a182108cdc379f
lihaochen <haochenli2000@qq.com>
2017-08-29 16:42:18 +0800

    添加可变的构建参数

--- app/src/main/java/com/lhc/jenkinsbuild/MainActivity.java
+++ app/src/main/java/com/lhc/jenkinsbuild/MainActivity.java
@@ -2,12 +2,16 @@ package com.lhc.jenkinsbuild;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tv_test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv_test = (TextView) findViewById(R.id.tv_test);
+        tv_test.setText(BuildConfig.MARKER_URL);
     }
 }
